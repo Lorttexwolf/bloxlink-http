0e731ccc079c3a4375153db020afc09587bc4f2f
Lorttexwolf <comisgammer@gmail.com>
2023-09-22 21:19:06 -0500

    Fix image_server authorization & avatar

--- src/resources/images.py
+++ src/resources/images.py
@@ -2,7 +2,7 @@ import datetime
 from io import BytesIO
 from typing import Optional
 
-from resources.secrets import IMAGE_SERVER_HOST  # type: ignore
+from resources.secrets import IMAGE_SERVER_HOST, IMAGE_SERVER_AUTH  # type: ignore
 from resources.utils import fetch, ReturnType
 
 
@@ -10,10 +10,10 @@ from resources.utils import fetch, ReturnType
 async def fetch_card(avatar_img: str, joined_at: Optional[datetime.datetime] = None, background: Optional[str] = None):
     data, res = await fetch("GET", f"{IMAGE_SERVER_HOST}/card", return_data=ReturnType.BYTES, 
         headers={
-            "Authorization": "oof" # TODO: Move to fetch util
+            "Authorization": IMAGE_SERVER_AUTH # TODO: Move to fetch util
         }, 
         body={
-            "full_avatar_url": avatar_img,
+            "avatar_url": avatar_img,
             "joined_at": joined_at,
             "background": background
     })  # type: ignore
